559f3a5eaa2a64aa59e9c8b8b8bac39a88484f25
Barry Anil <barryanil1@gmail.com>
2024-01-03 19:08:41 +0530

    Add oAuth

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+      domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
+    }
+  }
+  
+  module.exports = nextConfig
